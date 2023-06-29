1001402e8d6c63ff84511b03a13b3ca8801ffcc4
Aaron Hall <ahall@pn2305209.lanl.gov>
2023-06-29 15:43:32 -0600

    Added assert_allclose for float comparison 	modified:   viral_seq/tests/test_features.py

--- viral_seq/tests/test_features.py
+++ viral_seq/tests/test_features.py
@@ -3,6 +3,7 @@ from pathlib import Path
 from viral_seq.analysis.spillover_predict import _append_recs
 from time import perf_counter
 import pandas as pd
+from numpy.testing import assert_allclose
 
 
 def test_features():
@@ -16,10 +17,9 @@ def test_features():
     df = pd.DataFrame(result, index=["NC_019843.3"]).reset_index()
     # Check our features match published results for NC_019843.3 (MERS-CoV)
     for column in df_test:
-        test_val = df_test[column].values[0]
-        res_val = df[column].values[0]
-        if isinstance(res_val, str):
+        test_val = df_test[column]
+        res_val = df[column]
+        if isinstance(test_val, str):
             assert test_val == res_val
-        elif isinstance(res_val, float):
-            # Comparison data only goes to 9 decimal places
-            assert test_val == float(format(res_val, ".9f"))
+        elif isinstance(test_val, float):
+            assert_allclose(test_val, res_val, rtol=1e-9, atol=0)

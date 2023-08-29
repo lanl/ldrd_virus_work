fe92556f7be2c9e141a5d28a33f59d9ce539a79f
Aaron Hall <ahall@pn2305209.lanl.gov>
2023-08-29 08:55:09 -0600

    Addressing open comments on MR and misc. housekeeping
    1. Removed concurrency in load_from_cache and build_table functions in spillover_predict.py
    2. Adjusted implementation of pytest marker slow for portability
    3. Removed main command from cli and corresponding function in spillover_predict.py
    4. Added pytest to mypy ignore
    5. Updated pre-commit linting/testing procedure

--- README.md
+++ README.md
@@ -3,13 +3,17 @@
 ## LDRD DR Computational Work Repo
 
 **Pre-Commit Procedure**
+Linting pre-commit procedure prevents unnecessary CI/CD failures, but testing procedure is necessary as tests marked slow will not run in CI/CD. These must be run in pre-commit.
+
 - Linting
 ```
 >black viral_seq
 >ruff check viral_seq --fix
+>mypy -p viral_seq
 ```
 
 - Testing
 ```
->pytest viral_seq
+>cd /tmp
+>python3 -m pytest --pyargs viral_seq
 ```
--- viral_seq/cli/cli.py
+++ viral_seq/cli/cli.py
@@ -1,4 +1,3 @@
-from time import perf_counter
 import rich_click as click
 import viral_seq.analysis.spillover_predict as sp
 import pandas as pd
@@ -42,56 +41,6 @@ _option_cache = [
 @click.group()
 def cli():
     pass
-
-
-# --- main ---
-@cli.command()
-@shared_options(_option_email)
-@click.option(
-    "--download_recs",
-    type=int,
-    show_default=True,
-    default=0,
-    help=(
-        "Specify a number of sequence records "
-        "to download (per search category), either generating "
-        "or expanding the local cache with records it doesn't "
-        "already have."
-    ),
-)
-@click.option(
-    "--save_model",
-    is_flag=True,
-    show_default=True,
-    default=False,
-    help=(
-        "Serialize (pickle) the trained ML model(s) to"
-        " disk so that they can be packaged/reused elsewhere"
-    ),
-)
-@click.option(
-    "--load_model",
-    is_flag=True,
-    show_default=True,
-    default=False,
-    help=(
-        "Load the trained (serialized/pickled) ML model(s) in to"
-        " memory so that they can be used immediately in cases where"
-        " it is not necessary to iterate on the model proper. This will"
-        " require that `save_model` has been used locally first."
-    ),
-)
-def main(download_recs, email, save_model, load_model):
-    start_sec = perf_counter()
-    sp.main(
-        download_recs=download_recs,
-        email=email,
-        save_model=save_model,
-        load_model=load_model,
-    )
-    end_sec = perf_counter()
-    execution_time_sec = end_sec - start_sec
-    print(f"viral_seq execution time (s): {execution_time_sec:.2f}")
 
 
 # --- search-data ---

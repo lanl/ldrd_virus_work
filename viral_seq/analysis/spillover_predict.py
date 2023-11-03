1eb06c01317e2e8ff190a59a3cd216adca06ba08
Aaron Hall <ahall@pn2305209.lanl.gov>
2023-11-03 09:51:12 -0600

    corrected random_state default values again

--- viral_seq/analysis/spillover_predict.py
+++ viral_seq/analysis/spillover_predict.py
@@ -294,7 +294,7 @@ def _grab_features(features, records, genomic, kmers, kmer_k, gc, kmers_pc, kmer
     return features
 
 
-def univariate_selection(X, y, uni_type, num_select, random_state=132456789):
+def univariate_selection(X, y, uni_type, num_select, random_state=123456789):
     np.random.seed(random_state)  # for mutual_info_classif
     uni_type = getattr(sklearn.feature_selection, uni_type)
     sel_ = SelectKBest(uni_type, k=num_select).fit(X, y)

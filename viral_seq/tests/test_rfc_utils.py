2dbcee6653b6c85f81a9a28bf74b10a4210b7e32
Aaron Hall <ahall@pn2305209.lanl.gov>
2024-01-03 15:39:40 -0700

    test_oob_score use assert_allclose, n_jobs=2

--- viral_seq/tests/test_rfc_utils.py
+++ viral_seq/tests/test_rfc_utils.py
@@ -2,11 +2,12 @@ from sklearn.ensemble import RandomForestClassifier
 from sklearn.datasets import make_classification
 from viral_seq.analysis import rfc_utils
 from sklearn.metrics import accuracy_score
+from numpy.testing import assert_allclose
 
 
 def test_oob_score():
     X, y = make_classification(n_samples=10, n_features=10, random_state=0)
     clf = RandomForestClassifier(oob_score=True)
     clf.fit(X, y)
-    test_oob_score = rfc_utils.oob_score(clf, X, y, accuracy_score)
-    assert clf.oob_score_ == test_oob_score
+    test_oob_score = rfc_utils.oob_score(clf, X, y, accuracy_score, n_jobs=2)
+    assert_allclose(test_oob_score, clf.oob_score_)

cd104f7f678093fc75ab4c30bbe6cf65937921db
Aaron Hall <ahall@pn2305209.lanl.gov>
2024-05-07 14:23:58 -0600

    ENH: optimization moved out of `rfc_utils`
    1. optimization now in `viral_seq.analyis.classifier`, tests moved as appropriate
    2. cross validation had to be adjusted due a previously known issue about unshared kmers
    3. `rfc_utils` passed to `classifier.get_hyperparameters` and contains information needed that is specific to `RandomForestClassifier`. Something similar will need to be passed for other models
    4. moved `distributions` initilization to `run_workflow`
    5. parameters are passed to the model and to `bayes_opt.BayesianOptimization.maximize` in more clearly named parameters `model_parameters`, `bayes_parameters`
    6. comment added about C long issue in tests for clarity

---
+++ viral_seq/analysis/classifier.py
@@ -0,0 +1,111 @@
+import numpy as np
+import numpy.typing as npt
+import pandas as pd
+from functools import partial
+from bayes_opt import BayesianOptimization
+from typing import Union, Any, Literal
+from viral_seq.analysis import spillover_predict as sp
+from joblib import Parallel, delayed
+from sklearn.metrics import get_scorer
+from sklearn.model_selection import StratifiedKFold
+
+
+def _cv_score_child(model, X, y, scoring, train, test, **kwargs) -> float:
+    X_train = X.iloc[train]
+    # After splitting data, kmers may no longer be shared in training
+    X_train = sp.drop_unshared_kmers(X_train)
+    X_test = X.iloc[test]
+    X_test = X_test[X_train.columns]
+    clf = model(**kwargs)
+    clf.fit(X_train, y[train])
+    scorer = get_scorer(scoring)
+    return scorer(clf, X_test, y[test])
+
+
+def cv_score(
+    model_utils,
+    X,
+    y,
+    n_splits=5,
+    scoring: str = "roc_auc",
+    kind: Literal["mean", "min"] = "min",
+    n_jobs_cv: int = 1,
+    **kwargs,  # classifier arguments
+) -> float:
+    for param in model_utils._floatparam_names:
+        if param in kwargs and isinstance(kwargs[param], float):
+            kwargs[param] = model_utils._floatparam(param, kwargs[param])
+    cv = StratifiedKFold(n_splits=n_splits)
+    scores = []
+    r = Parallel(n_jobs=n_jobs_cv)(
+        delayed(_cv_score_child)(
+            model_utils._model, X, y, scoring, train, test, **kwargs
+        )
+        for train, test in cv.split(X, y)
+    )
+    for res in r:
+        scores.append(res)
+    if kind == "mean":
+        return np.mean(scores)
+    elif kind == "min":
+        return np.min(scores)
+
+
+def get_hyperparameters(
+    model_utils,
+    X: pd.DataFrame,
+    y: npt.ArrayLike,
+    distributions: dict[str, tuple[float, float]],
+    model_parameters: dict[str, Any] = {},
+    bayes_parameters: dict[str, Any] = {},
+    random_state: int = 0,
+    n_jobs_cv: int = 1,
+) -> dict[str, Union[float, dict[str, Any]]]:
+    """Search for the best hyperparameters using bayesian optimization
+
+    Parameters:
+        model_utils: helper module for each model, see use below and `viral_seq.analysis.rfc_utils`
+        X (pd.DataFrame), y (npt.ArrayLike): data used for model evaluation
+        distributions (dict[str, tuple[float, float]]): dictionary of parameters to optimize
+        model_parameters: passed as is to the model used for scoring. These are not optimized and should not overlap with parameters in `distributions`
+        bayes_parameters: passed as is to `bayes_opt.BayesianOptimization.maximize`
+        random_state (int): seed used when needed for repeatability
+        n_jobs_cv (int): used for `sklearn.model_selection.cross_val_score`, pass n_jobs to model training with `model_parameters`
+
+    Returns:
+        (dict[str, Union[float, dict[str, Any]]]): dictionary with best performing score (key 'target'), parameters (key 'params'), and target history during optimization (key 'targets')
+    """
+    duplicate_params = set(distributions.keys()).intersection(set(model_parameters))
+    if len(duplicate_params) > 0:
+        raise ValueError(
+            "Received inputs for the following parameters in both `distributions` and `model_parameters`: "
+            + str(duplicate_params)
+            + ". Model parameters to be optimized should be passed in `distributions` and those to be set but not optimized in `model_parameters`."
+        )
+    optimizer = BayesianOptimization(
+        partial(
+            cv_score,
+            model_utils,
+            X=X,
+            y=y,
+            kind="min",
+            n_jobs_cv=n_jobs_cv,
+            random_state=random_state,
+            **model_parameters,
+        ),
+        distributions,
+        random_state=random_state,
+    )
+    # check default settings first
+    defaults = model_utils._default_parameters(X.shape)
+    # only include what's in the parameter space
+    defaults = {key: defaults[key] for key in distributions.keys()}
+    optimizer.probe(params=defaults, lazy=True)
+    optimizer.maximize(**bayes_parameters)
+    res = optimizer.max
+    for name in model_utils._floatparam_names:
+        if name in res["params"]:
+            res["params"][name] = model_utils._floatparam(name, res["params"][name])
+    targets = list(pd.DataFrame(optimizer.res)["target"].values)
+    res["targets"] = targets
+    return res
--- viral_seq/tests/test_rfc_utils.py
+++ viral_seq/tests/test_rfc_utils.py
@@ -2,8 +2,7 @@ from sklearn.ensemble import RandomForestClassifier
 from sklearn.datasets import make_classification
 from viral_seq.analysis import rfc_utils
 from sklearn.metrics import accuracy_score, roc_auc_score, balanced_accuracy_score
-import numpy as np
-from numpy.testing import assert_allclose, assert_array_equal
+from numpy.testing import assert_allclose
 import pandas as pd
 import pytest
 from hypothesis import example, given, strategies as st
@@ -81,100 +80,12 @@ def test_floatparams(val, name):
         RandomForestClassifier(**{name: rfc_utils._floatparam(name, val)})
 
 
-@pytest.mark.parametrize("random_state, score", [(0, 1.0), (1, 1.0), (2, 0.25)])
-def test_min_cv_score(random_state, score):
-    X, y = make_classification(n_samples=10, n_features=10, random_state=random_state)
-    test_score = rfc_utils.min_cv_score(
-        X, y, cv=3, n_estimators=10, random_state=random_state
-    )
-    assert_allclose(test_score, score)
-
-
 @given(
-    max_depth=st.floats(
-        min_value=0.0, max_value=100.99999, allow_nan=False, allow_infinity=False
-    ),
-    criterion=st.floats(
-        min_value=0.0, max_value=1.0, allow_nan=False, allow_infinity=False
-    ),
-    class_weight=st.floats(
-        min_value=0.0, max_value=1.0, allow_nan=False, allow_infinity=False
-    ),
-    floats=st.booleans(),
-)
-def test_min_cv_score_args(max_depth, criterion, class_weight, floats):
-    X, y = make_classification(n_samples=10, n_features=10)
-    if not floats:
-        max_depth = rfc_utils._floatparam("max_depth", max_depth)
-        criterion = rfc_utils._floatparam("criterion", criterion)
-        class_weight = rfc_utils._floatparam("class_weight", class_weight)
-    rfc_utils.min_cv_score(
-        X,
-        y,
-        cv=3,
-        max_depth=max_depth,
-        criterion=criterion,
-        class_weight=class_weight,
-        n_estimators=10,
-    )
-
-
-@pytest.mark.parametrize(
-    "max_depth, criterion, class_weight",
-    [
-        ("bad value", "gini", None),
-        (1, "bad value", None),
-        (1, "gini", "bad value"),
-    ],
-)
-def test_min_cv_score_args_passed(max_depth, criterion, class_weight):
-    X, y = make_classification(n_samples=10, n_features=10)
-    with pytest.raises(ValueError, match="InvalidParameterError"):
-        rfc_utils.min_cv_score(
-            X,
-            y,
-            cv=3,
-            max_depth=max_depth,
-            criterion=criterion,
-            class_weight=class_weight,
-            n_estimators=10,
-        )
-
-
-@pytest.mark.parametrize(
-    "distributions, score, params",
-    [
-        (
-            None,
-            1.0,
-            {
-                "class_weight": None,
-                "criterion": "gini",
-                "max_depth": None,
-                "max_features": 0.31622776601683794,
-                "max_samples": 1.0,
-                "min_samples_leaf": 0.1,
-                "min_samples_split": 0.2,
-            },
-        ),
-        ({"max_samples": (0.0, 0.5)}, 1.0, {"max_samples": 1.0}),
-    ],
-)
-def test_get_hyperparameters(distributions, score, params):
-    X, y = make_classification(n_samples=10, n_features=10, random_state=0)
-    res = rfc_utils.get_hyperparameters(
-        X,
-        y,
-        init_points=1,
-        n_iter=1,
-        n_jobs=1,
-        random_state=0,
-        distributions=distributions,
-        n_estimators=10,
-    )
-    assert res["target"] == pytest.approx(score)
-    assert_array_equal(
-        np.sort(list(res["params"].keys())), np.sort(list(params.keys()))
-    )
-    for key in res["params"].keys():
-        assert res["params"][key] == pytest.approx(params[key])
+    x=st.integers(), y=st.integers(max_value=9.223372036854776e18)
+)  # max_value set to C long max, max accepted by numpy
+def test_defaults_default_param_argument_guards(x, y):
+    if x <= 0 or y <= 0:
+        with pytest.raises(ValueError, match="must be positive"):
+            rfc_utils._default_parameters((x, y))
+    else:
+        rfc_utils._default_parameters((x, y))

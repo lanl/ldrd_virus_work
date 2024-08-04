c078a99348fbd95d8d8e62bcf4ed52ba481a5dde
Tyler Reddy <treddy@lanl.gov>
2024-08-04 11:23:39 -0600

    ENH: update organism_dict
    * Add more virus:host mappings to `organism_dict`, bringing
    us to Mollentze training set record 230.

--- retarget.py
+++ retarget.py
@@ -781,6 +781,27 @@ organism_dict = {
                 # https://doi.org/10.1016%2Fj.virusres.2023.199186
                 # controversial--evidence of human infection...
                 "Bovine leukemia virus": "human",
+                # https://ictv.global/report/chapter/poxviridae/poxviridae/parapoxvirus
+                # https://www.genome.jp/virushostdb/1579460
+                "Parapoxvirus red deer/HL953": "non_primate_mammals",
+                # https://www.genome.jp/virushostdb/238817
+                # I don't count Vero cells as sufficient evidence for
+                # primate infection though
+                # https://doi.org/10.3851%2FIMP1729
+                # Maporal is intentionally used in hantavirus studies
+                # to avoid BSL-4 requirements since there's no evidence
+                # it can infect humans
+                "Maporal virus": "non_primate_mammals",
+                # https://www.genome.jp/virushostdb/35314
+                # mammal infection evidence not conclusive yet it seems:
+                # https://doi.org/10.3390/v7112918
+                "Koongol virus": "no_mammals",
+                # https://www.genome.jp/virushostdb/50292
+                # https://en.wikipedia.org/wiki/Cercopithecine_betaherpesvirus_5
+                # viruses in this family do seem to be able to infect humans,
+                # but I don't see concrete evidence of "strain 5" infecting
+                # humans so I'm not confident enough...
+                "Cercopithecine betaherpesvirus 5": "primate",
                 }
 
 
@@ -864,7 +885,7 @@ def main(cache_path):
     df_train = pd.read_csv("viral_seq/data/Mollentze_Training.csv")
     y_human_train, y_mammal_train, y_primate_train = retarget(df=df_train,
                                                               cache_path=cache_path,
-                                                              n_records=225)
+                                                              n_records=230)
     df_test = pd.read_csv("viral_seq/data/Mollentze_Holdout.csv")
     y_human_test, y_mammal_test, y_primate_test = retarget(df=df_test,
                                                            cache_path=cache_path,

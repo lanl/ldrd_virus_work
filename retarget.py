616c8a244581ab419dd29ffcdbdf61a0d9b734c8
Tyler Reddy <treddy@lanl.gov>
2024-09-08 13:47:26 -0600

    ENH: update organism_dict
    * Add three new virus:host mappings to `organism_dict`,
    bringing us to Mollentze test set record 290 in the
    remapping effort.

--- retarget.py
+++ retarget.py
@@ -940,6 +940,16 @@ organism_dict = {
                 # as strong enough, so counting as bats/mammals:
                 # https://www.genome.jp/virushostdb/1554500
                 "Hervey virus": "non_primate_mammals",
+                # https://www.genome.jp/virushostdb/2021721
+                # don't see evidence beyond mosquito
+                "Holmes Jungle virus": "no_mammals",
+                # https://www.genome.jp/virushostdb/1673638
+                # https://ictv.global/report/chapter/circoviridae/circoviridae/cyclovirus
+                "Cyclovirus Equ1": "non_primate_mammals",
+                # may be a "naked virus" per
+                # https://doi.org/10.1016/j.actatropica.2024.107158
+                # https://www.genome.jp/virushostdb/1608048
+                "Huangpi Tick Virus 2": "no_mammals",
                 }
 
 
@@ -1027,7 +1037,7 @@ def main(cache_path):
     df_test = pd.read_csv("viral_seq/data/Mollentze_Holdout.csv")
     y_human_test, y_mammal_test, y_primate_test = retarget(df=df_test,
                                                            cache_path=cache_path,
-                                                           n_records=285)
+                                                           n_records=290)
     np.savez("relabeled_data.npz",
              y_human_train=y_human_train,
              y_mammal_train=y_mammal_train,

cbe4ee67a828c9f39b58220b7de57373a84d18a5
Abhishek Mallela <amallela@lanl.gov>
2024-09-12 12:13:33 -0600

    MAINT: Addressed reviewer comments
    Adjusted the `csv_conversion` function to allow for an "Is_Both" option and modified the regression test to be sensitive to changes in logic.

--- viral_seq/run_workflow.py
+++ viral_seq/run_workflow.py
@@ -61,14 +61,21 @@ def csv_conversion(input_csv: str = "receptor_training.csv") -> pd.DataFrame:
             "Receptor_Type": "Is_Integrin",
         }
     )
+    table["Is_Both"] = np.where(table["Is_Integrin"] == "both", True, False)
     table["Is_Sialic_Acid"] = pd.Series(dtype="bool")
     table = table[
-        ["Species", "Accessions", "Human Host", "Is_Integrin", "Is_Sialic_Acid"]
+        [
+            "Species",
+            "Accessions",
+            "Human Host",
+            "Is_Integrin",
+            "Is_Sialic_Acid",
+            "Is_Both",
+        ]
     ]
     table["Is_Sialic_Acid"] = np.where(table["Is_Integrin"] == "integrin", False, True)
     table["Is_Integrin"] = np.where(table["Is_Integrin"] == "sialic_acid", False, True)
-    output_df = table
-    return output_df
+    return table
 
 
 def validate_feature_table(file_name, idx, prefix):

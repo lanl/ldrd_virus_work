ca18d82e7afb12fda7d2ab75bfcee2e195b26563
Tyler Reddy <treddy@lanl.gov>
2024-09-14 11:08:03 -0600

    ENH: update organism_dict
    * Add five more virus:host mappings to `organism_dict`,
    bringing us to Mollentze test set record 335 in the remapping
    effort.

--- retarget.py
+++ retarget.py
@@ -979,6 +979,21 @@ organism_dict = {
                 # solid evidence of primate infection
                 # (prefer actual cases of infection)
                 "Ixcanal virus": "no_mammals",
+                # https://en.wikipedia.org/wiki/Jamestown_Canyon_encephalitis
+                "Jamestown Canyon virus": "human",
+                # https://doi.org/10.1016/S0168-1702(01)00262-3
+                # https://www.genome.jp/virushostdb/150058
+                "Jatobal virus": "non_primate_mammals",
+                # https://www.genome.jp/virushostdb/322067
+                # https://ictv.global/report/chapter/paramyxoviridae/paramyxoviridae/jeilongvirus
+                "J virus": "non_primate_mammals",
+                # https://www.genome.jp/virushostdb/1851087
+                # https://doi.org/10.1292%2Fjvms.21-0577
+                "Kabuto mountain virus": "human",
+                # https://www.genome.jp/virushostdb/35514
+                # https://en.wikipedia.org/wiki/Keystone_virus
+                # https://doi.org/10.4269/ajtmh.22-0594
+                "Keystone virus": "human",
                 }
 
 
@@ -1066,7 +1081,7 @@ def main(cache_path):
     df_test = pd.read_csv("viral_seq/data/Mollentze_Holdout.csv")
     y_human_test, y_mammal_test, y_primate_test = retarget(df=df_test,
                                                            cache_path=cache_path,
-                                                           n_records=330)
+                                                           n_records=335)
     np.savez("relabeled_data.npz",
              y_human_train=y_human_train,
              y_mammal_train=y_mammal_train,

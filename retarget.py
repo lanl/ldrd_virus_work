6073d094d9f504271c273bd1d058086dc4db9532
Tyler Reddy <treddy@lanl.gov>
2024-09-20 10:33:57 -0600

    ENH: update organism_dict
    * Add 5 new virus:host mappings to `organism_dict`, bringing us
    to Mollentze test set record 370 in the remapping effort.

--- retarget.py
+++ retarget.py
@@ -1081,6 +1081,19 @@ organism_dict = {
                 # https://www.genome.jp/virushostdb/11575
                 # https://doi.org/10.3201%2Feid2308.161254
                 "Maguari virus": "human",
+                # https://www.genome.jp/virushostdb/2170064
+                # https://ictv.global/report/chapter/spinareoviridae/spinareoviridae/orthoreovirus
+                # I don't usually accept cell culture line evidence
+                "Mahlapitsi orthoreovirus": "no_mammals",
+                # https://www.genome.jp/virushostdb/1603963
+                # https://ictv.global/report/chapter/picornaviridae/picornaviridae/malagasivirus
+                "malagasivirus A1": "primate",
+                # https://www.genome.jp/virushostdb/1603964
+                "malagasivirus B1": "primate",
+                # https://www.genome.jp/virushostdb/1985386
+                "Mallard associated gemycircularvirus 1": "avian",
+                # https://www.genome.jp/virushostdb/1843734
+                "Faeces associated gemycircularvirus 14": "avian",
                 }
 
 
@@ -1168,7 +1181,7 @@ def main(cache_path):
     df_test = pd.read_csv("viral_seq/data/Mollentze_Holdout.csv")
     y_human_test, y_mammal_test, y_primate_test = retarget(df=df_test,
                                                            cache_path=cache_path,
-                                                           n_records=365)
+                                                           n_records=370)
     np.savez("relabeled_data.npz",
              y_human_train=y_human_train,
              y_mammal_train=y_mammal_train,

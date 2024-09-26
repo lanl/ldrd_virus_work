1e3fe17753aef08d1f9ad87516d4b7a23a90391f
Tyler Reddy <treddy@lanl.gov>
2024-09-26 16:34:17 -0600

    ENH: update organism_dict
    * Add 5 new virus:host mappings to `organism_dict`, bringing us
    to Mollentze test set record 430 in the remapping effort.

--- retarget.py
+++ retarget.py
@@ -1238,6 +1238,20 @@ organism_dict = {
                 # https://www.genome.jp/virushostdb/590647
                 # https://ictv.global/report/chapter/paramyxoviridae/paramyxoviridae/narmovirus
                 "Nariva virus": "non_primate_mammals",
+                # https://www.genome.jp/virushostdb/38170
+                # https://en.wikipedia.org/wiki/Avian_orthoreovirus
+                # (ignore cell culture host range)
+                "Avian orthoreovirus": "avian",
+                # https://www.genome.jp/virushostdb/629739
+                "Nique virus": "no_mammals",
+                # https://www.genome.jp/virushostdb/2569589
+                # https://doi.org/10.3201%2Feid2504.180750
+                # https://doi.org/10.1155%2F2022%2F4231978
+                "Ntepes virus": "human",
+                # https://www.genome.jp/virushostdb/1503291
+                "BtNv-AlphaCoV/SC2013": "non_primate_mammals",
+                # https://www.genome.jp/virushostdb/318852
+                "Oak-Vale virus": "no_mammals",
                 }
 
 
@@ -1325,7 +1339,7 @@ def main(cache_path):
     df_test = pd.read_csv("viral_seq/data/Mollentze_Holdout.csv")
     y_human_test, y_mammal_test, y_primate_test = retarget(df=df_test,
                                                            cache_path=cache_path,
-                                                           n_records=425)
+                                                           n_records=430)
     np.savez("relabeled_data.npz",
              y_human_train=y_human_train,
              y_mammal_train=y_mammal_train,

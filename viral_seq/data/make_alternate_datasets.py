1194952e40c6a3523948f544957aaff5ec4dd1f3
Aaron Hall <ahall@pn2305209.lanl.gov>
2024-10-09 10:22:47 -0600

    ENH: make rebalanced dataset from fixed dataset in !33

--- viral_seq/data/make_alternate_datasets.py
+++ viral_seq/data/make_alternate_datasets.py
@@ -3,6 +3,7 @@ from viral_seq.analysis import spillover_predict as sp
 from importlib.resources import files
 from pathlib import Path
 import tarfile
+import numpy as np
 
 
 def get_bad_indexes(df, cache, train_accessions=None):
@@ -38,6 +39,45 @@ def get_bad_indexes(df, cache, train_accessions=None):
     }
 
 
+def shuffle(df_train, df_test, random_state=2930678936):
+    train_count = df_train.shape[0]
+    train_ratio = df_train["Human Host"].value_counts()[True] / train_count
+    # put all data together
+    all_data = pd.concat([df_train, df_test])
+    # use a martini method to randomly add a virus from all data (without replacement) to a new training set if the new virus moves the target ratio of the new training set closer to that of the original training data
+    # this should ensure we have the same amount of "Human Host" in the new set as the old set
+    df_train_new = pd.DataFrame(columns=all_data.columns)
+    df_test_new = pd.DataFrame(columns=all_data.columns)
+    indexes = [i for i in range(all_data.shape[0])]
+    rng = np.random.default_rng(random_state)
+    for i in range(train_count):
+        if i > 0:
+            value_counts = df_train_new["Human Host"].value_counts()
+            true_counts = 0 if True not in value_counts.index else value_counts[True]
+            train_ratio_new = true_counts / (i + 1)
+        while True:
+            this_idx = rng.choice(indexes)
+            this_row = all_data.iloc[this_idx]
+            new_idx = len(df_train_new.index)
+            if i == 0:
+                df_train_new.loc[new_idx] = this_row
+                indexes.pop(indexes.index(this_idx))
+                break
+            else:
+                this_bool = train_ratio_new < train_ratio
+                if this_row["Human Host"] == this_bool:
+                    df_train_new.loc[new_idx] = this_row
+                    indexes.pop(indexes.index(this_idx))
+                    break
+    # everything left is test
+    for this_idx in indexes:
+        this_row = all_data.iloc[this_idx]
+        new_idx = len(df_test_new.index)
+        df_test_new.loc[new_idx] = this_row
+
+    return df_train_new, df_test_new
+
+
 if __name__ == "__main__":
     data = files("viral_seq.data")
     train_data = str(data.joinpath("Mollentze_Training.csv"))
@@ -48,6 +88,8 @@ if __name__ == "__main__":
     cache_viral = Path("data_external/cache_viral")
     train_fixed_data = Path("Mollentze_Training_Fixed.csv")
     test_fixed_data = Path("Mollentze_Holdout_Fixed.csv")
+    train_shuffled_data = Path("Mollentze_Training_Shuffled.csv")
+    test_shuffled_data = Path("Mollentze_Holdout_Shuffled.csv")
     train_accessions: set[str] = set()
 
     with tarfile.open(cache_file, "r") as tar:
@@ -80,4 +122,16 @@ if __name__ == "__main__":
         "Generated 'fixed' datasets with no partials, no genomes without 'good' CDS, and duplicates dropped from test data:",
         train_fixed_data,
         test_fixed_data,
+    )
+
+    train_data_shuffled, test_data_shuffled = shuffle(
+        df_train_fixed, df_test_fixed, random_state=2930678936
+    )
+    train_data_shuffled.to_csv(train_shuffled_data)
+    test_data_shuffled.to_csv(test_shuffled_data)
+
+    print(
+        "Generated 'shuffled' datasets from 'fixed' datasets where viral genomes have been randomly sorted to train and test while preserving the number of viruses in each set and the human host ratio of each set:",
+        train_shuffled_data,
+        test_shuffled_data,
     )
--- viral_seq/tests/test_data.py
+++ viral_seq/tests/test_data.py
@@ -1,5 +1,6 @@
 from viral_seq.data import make_alternate_datasets as make_alts
 import pandas as pd
+from pandas.testing import assert_frame_equal
 from importlib.resources import files
 import pytest
 
@@ -48,3 +49,37 @@ def test_get_bad_indexes(cache, accession, train_accessions, expected_result):
     result = make_alts.get_bad_indexes(df, this_cache, train_accessions)
     for key in result:
         assert result[key] == expected_result[key]
+
+
+@pytest.mark.parametrize("random_state", [123456, 203254, 987654])
+def test_shuffle(random_state):
+    train_data = files("viral_seq.tests") / "TrainingSet.csv"
+    test_data = files("viral_seq.tests") / "TestSet.csv"
+    df_train = pd.read_csv(train_data, index_col=0)
+    df_test = pd.read_csv(test_data, index_col=0)
+    df_train_shuffled, df_test_shuffled = make_alts.shuffle(
+        df_train, df_test, random_state
+    )
+
+    # assert count of "Human Host" hasn't changed
+    for df1, df2 in zip([df_train_shuffled, df_test_shuffled], [df_train, df_test]):
+        for truth in [True, False]:
+            assert (
+                df1["Human Host"].value_counts()[truth]
+                == df2["Human Host"].value_counts()[truth]
+            )
+
+    # assert we have changed the tables in some way
+    with pytest.raises(AssertionError):
+        assert_frame_equal(df_train, df_train_shuffled, check_dtype=False)
+    with pytest.raises(AssertionError):
+        assert_frame_equal(df_test, df_test_shuffled, check_dtype=False)
+
+    # assert we still have the same data present
+    original_data = pd.concat([df_train, df_test]).sort_values(
+        by="Species", ignore_index=True
+    )
+    shuffled_data = pd.concat([df_train_shuffled, df_test_shuffled]).sort_values(
+        by="Species", ignore_index=True
+    )
+    assert_frame_equal(original_data, shuffled_data, check_dtype=False)

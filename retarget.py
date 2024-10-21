17bff588532a7f1b67091f35f7345e5c85b06494
Tyler Reddy <treddy@lanl.gov>
2024-10-21 14:44:20 -0600

    ENH: update organism_dict
    * Add 3 new virus:host mappings to `organism_dict`, bringing
    us to Mollentze test set record 490 in the remapping effort.

--- retarget.py
+++ retarget.py
@@ -1388,6 +1388,15 @@ organism_dict = {
                 # https://doi.org/10.4269/ajtmh.1998.59.704 (deer)
                 # https://doi.org/10.7589/0090-3558-32.3.444 (deer)
                 "Potosi virus": "non_primate_mammals",
+                # https://www.genome.jp/virushostdb/487097
+                # scanning the literature, I'm only convinced
+                # of infection of ticks... not animals (yet)
+                "Precarious point virus": "no_mammals",
+                # https://www.genome.jp/virushostdb/1581151
+                "Slow loris parvovirus 1": "primate",
+                # https://www.genome.jp/virushostdb/1219896
+                # https://doi.org/10.3201/eid1901.121078
+                "Raccoon polyomavirus": "non_primate_mammals",
                 }
 
 
@@ -1475,7 +1484,7 @@ def main(cache_path):
     df_test = pd.read_csv("viral_seq/data/Mollentze_Holdout.csv")
     y_human_test, y_mammal_test, y_primate_test = retarget(df=df_test,
                                                            cache_path=cache_path,
-                                                           n_records=485)
+                                                           n_records=490)
     np.savez("relabeled_data.npz",
              y_human_train=y_human_train,
              y_mammal_train=y_mammal_train,

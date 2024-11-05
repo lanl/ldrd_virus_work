6d77d0d683c0138566e1e2a489d06c8cced50ead
Tyler Reddy <treddy@lanl.gov>
2024-11-05 16:10:23 -0700

    ENH: update organism_dict
    * Add 5 new virus:host mappings to `organism_dict`, bringing us
    to Mollentze test set record 564 in the remapping effort.

--- retarget.py
+++ retarget.py
@@ -1569,6 +1569,21 @@ organism_dict = {
                 "Rousettus aegyptiacus polyomavirus 1": "non_primate_mammals",
                 # https://www.genome.jp/virushostdb/1892416
                 "Rousettus bat coronavirus": "non_primate_mammals",
+                # https://www.genome.jp/virushostdb/3052326
+                # https://ictv.global/report/chapter/arenaviridae/arenaviridae/mammarenavirus
+                # many mammarenaviruses have been reported to occasionally infect
+                # exposed humans, but I don't see immediate evidence for
+                # this one
+                "Mammarenavirus ryukyuense": "non_primate_mammals",
+                # https://www.genome.jp/virushostdb/159138
+                "Sabo virus": "non_primate_mammals",
+                # https://www.genome.jp/virushostdb/452475
+                "Squirrel monkey polyomavirus": "primate",
+                # https://www.genome.jp/virushostdb/1236410
+                "Saimiri sciureus polyomavirus 1": "primate",
+                # https://www.genome.jp/virushostdb/1535247
+                # https://ictv.global/report/chapter/orthoherpesviridae/orthoherpesviridae/cytomegalovirus
+                "Saimiriine betaherpesvirus 4": "primate",
                 }
 
 
@@ -1656,7 +1671,7 @@ def main(cache_path):
     df_test = pd.read_csv("viral_seq/data/Mollentze_Holdout.csv")
     y_human_test, y_mammal_test, y_primate_test = retarget(df=df_test,
                                                            cache_path=cache_path,
-                                                           n_records=559)
+                                                           n_records=564)
     np.savez("relabeled_data.npz",
              y_human_train=y_human_train,
              y_mammal_train=y_mammal_train,

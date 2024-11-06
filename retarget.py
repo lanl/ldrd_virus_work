cf224a28b848fefe07efdc1916fbe40728fed76f
Tyler Reddy <treddy@lanl.gov>
2024-11-05 21:09:06 -0700

    ENH: update organism_dict
    * Add 4 new virus:host mappings to `organism_dict`, bringing
    us to Mollentze test set record 570 in the remapping effort.

--- retarget.py
+++ retarget.py
@@ -1589,6 +1589,14 @@ organism_dict = {
                 # https://www.genome.jp/virushostdb/520973
                 # https://ictv.global/report/chapter/caliciviridae/caliciviridae/valovirus
                 "Calicivirus pig/AB90/CAN": "non_primate_mammals",
+                # https://www.genome.jp/virushostdb/120499
+                "Salem virus": "non_primate_mammals",
+                # https://www.genome.jp/virushostdb/427316
+                "Salobo virus": "non_primate_mammals",
+                # https://www.genome.jp/virushostdb/45767
+                "San Angelo virus": "no_mammals",
+                # https://www.genome.jp/virushostdb/159152
+                "Sango virus": "non_primate_mammals",
                 }
 
 
@@ -1676,7 +1684,7 @@ def main(cache_path):
     df_test = pd.read_csv("viral_seq/data/Mollentze_Holdout.csv")
     y_human_test, y_mammal_test, y_primate_test = retarget(df=df_test,
                                                            cache_path=cache_path,
-                                                           n_records=566)
+                                                           n_records=570)
     np.savez("relabeled_data.npz",
              y_human_train=y_human_train,
              y_mammal_train=y_mammal_train,

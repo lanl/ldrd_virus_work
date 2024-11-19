d4130ae469ab50afebfc0517758b70fef1fe2d9e
Tyler Reddy <treddy@lanl.gov>
2024-11-19 12:57:56 -0700

    ENH: update organism_dict
    * Add 4 new virus:host mappings to `organism_dict`, bringing us
    to Mollentze test set record 588 in the remapping effort.

--- retarget.py
+++ retarget.py
@@ -1638,6 +1638,18 @@ organism_dict = {
                 # of these types of viruses so far could be
                 # from gut microorganisms rather than the host proper
                 "Sewage associated gemycircularvirus 3": "no_mammals",
+                # https://www.genome.jp/virushostdb/1519407
+                # again, no conclusive evidence for specific host
+                # for this sewage-isolated sample
+                "Sewage-associated gemycircularvirus 2": "no_mammals",
+                # https://www.genome.jp/virushostdb/1519399
+                "Sewage-associated gemycircularvirus 4": "no_mammals",
+                # https://www.genome.jp/virushostdb/1843736
+                "Faeces associated gemycircularvirus 16": "non_primate_mammals",
+                # https://www.genome.jp/virushostdb/291590
+                # https://ictv.global/report/chapter/papillomaviridae/papillomaviridae/sigmapapillomavirus
+                # https://doi.org/10.1016/j.virol.2004.10.033
+                "Erethizon dorsatum papillomavirus 1": "non_primate_mammals",
                 }
 
 
@@ -1725,7 +1737,7 @@ def main(cache_path):
     df_test = pd.read_csv("viral_seq/data/Mollentze_Holdout.csv")
     y_human_test, y_mammal_test, y_primate_test = retarget(df=df_test,
                                                            cache_path=cache_path,
-                                                           n_records=583)
+                                                           n_records=588)
     np.savez("relabeled_data.npz",
              y_human_train=y_human_train,
              y_mammal_train=y_mammal_train,

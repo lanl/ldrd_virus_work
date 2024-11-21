6c40daa91759f81767bb3a6cd3700a669acc94e7
Tyler Reddy <treddy@lanl.gov>
2024-11-21 15:34:39 -0700

    ENH: update organism_dict
    * Add 5 new virus:host mappings to `organism_dict`, bringing us
    to Mollentze test set record 593 in the remapping effort.

--- retarget.py
+++ retarget.py
@@ -1650,6 +1650,18 @@ organism_dict = {
                 # https://ictv.global/report/chapter/papillomaviridae/papillomaviridae/sigmapapillomavirus
                 # https://doi.org/10.1016/j.virol.2004.10.033
                 "Erethizon dorsatum papillomavirus 1": "non_primate_mammals",
+                # https://www.genome.jp/virushostdb/1564099
+                "Silverwater virus": "non_primate_mammals",
+                # https://www.genome.jp/virushostdb/38420
+                # https://ictv.global/report/chapter/adenoviridae/adenoviridae/mastadenovirus
+                "Simian adenovirus 3": "primate",
+                # https://www.genome.jp/virushostdb/995022
+                "Simian adenovirus 49": "primate",
+                # https://www.genome.jp/virushostdb/1298385
+                # https://doi.org/10.1128/mbio.00084-13
+                "Baboon adenovirus 3": "human",
+                # https://www.genome.jp/virushostdb/38432
+                "Simian adenovirus 13": "primate",
                 }
 
 
@@ -1737,7 +1749,7 @@ def main(cache_path):
     df_test = pd.read_csv("viral_seq/data/Mollentze_Holdout.csv")
     y_human_test, y_mammal_test, y_primate_test = retarget(df=df_test,
                                                            cache_path=cache_path,
-                                                           n_records=588)
+                                                           n_records=593)
     np.savez("relabeled_data.npz",
              y_human_train=y_human_train,
              y_mammal_train=y_mammal_train,

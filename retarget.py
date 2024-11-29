b2132772fe85455f3d58aba7c08d97a0062a264a
Tyler Reddy <treddy@lanl.gov>
2024-11-29 15:33:23 -0700

    ENH: update organism_dict
    * Add 5 new virus:host mappings to `organism_dict`, bringing us
    to Mollentze test set record 603 in the remapping effort.
    
    [ci skip]

--- retarget.py
+++ retarget.py
@@ -1672,6 +1672,21 @@ organism_dict = {
                 "Simian adenovirus DM-2014": "primate",
                 # https://www.genome.jp/virushostdb/2848082
                 "simian adenovirus 55": "primate",
+                # https://www.genome.jp/virushostdb/1520004
+                "Skua adenovirus 1": "avian",
+                # https://www.genome.jp/virushostdb/1641642
+                # https://doi.org/10.7589/JWD-D-21-00099
+                # https://doi.org/10.1016/j.virol.2015.06.026
+                "Skunk adenovirus PB1": "non_primate_mammals",
+                # https://www.genome.jp/virushostdb/160796
+                # I don't accept cell culture evidence for host range
+                "Skunkpox virus": "non_primate_mammals",
+                # https://www.genome.jp/virushostdb/11580
+                # https://doi.org/10.1093/jme/tjad128
+                "Snowshoe hare virus": "human",
+                # https://www.genome.jp/virushostdb/2050019
+                # https://ictv.global/report/chapter/coronaviridae/coronaviridae/orthocoronavirinae
+                "Shrew coronavirus": "non_primate_mammals",
                 }
 
 
@@ -1759,7 +1774,7 @@ def main(cache_path):
     df_test = pd.read_csv("viral_seq/data/Mollentze_Holdout.csv")
     y_human_test, y_mammal_test, y_primate_test = retarget(df=df_test,
                                                            cache_path=cache_path,
-                                                           n_records=598)
+                                                           n_records=603)
     np.savez("relabeled_data.npz",
              y_human_train=y_human_train,
              y_mammal_train=y_mammal_train,

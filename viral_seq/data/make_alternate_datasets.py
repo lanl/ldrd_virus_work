6e84ead21cd1f32a0fecfcc4c22a36f6fdcded7f
Aaron Hall <ahall@pn2305209.lanl.gov>
2024-12-03 09:59:03 -0700

    TST: Tests for `get_bad_indexes`
    Additionally, now uses stored cache. Made some code in
    `make_alternate_datasets` conditional on `if __name__ == '__main__'`

--- viral_seq/data/make_alternate_datasets.py
+++ viral_seq/data/make_alternate_datasets.py
@@ -3,42 +3,34 @@ from viral_seq.analysis import spillover_predict as sp
 from viral_seq.cli import cli
 from importlib.resources import files
 from pathlib import Path
+import tarfile
+
 
 data = files("viral_seq.data")
 train_data = str(data.joinpath("Mollentze_Training.csv"))
 test_data = str(data.joinpath("Mollentze_Holdout.csv"))
+cache_file = str(data.joinpath("cache_mollentze.tar.gz"))
+cache_extract_path = Path("data_external")
+cache_extract_path.mkdir(parents=True, exist_ok=True)
 cache_viral = Path("data_external/cache_viral")
-cache_viral.mkdir(parents=True, exist_ok=True)
 train_fixed_data = str(Path("Mollentze_Training_Fixed.csv"))
 test_fixed_data = str(Path("Mollentze_Holdout_Fixed.csv"))
-
 train_accessions: set[str] = set()
 email = "arhall@lanl.gov"
 
-for file in [train_data, test_data]:
-    cli.pull_data(
-        [
-            "--email",
-            email,
-            "--cache",
-            cache_viral,
-            "--file",
-            file,
-            "--no-filter",
-        ],
-        standalone_mode=False,
-    )
 
-
-def get_bad_indexes(df, test=False):
+def get_bad_indexes(df, cache, train_accessions=None):
     partial_lst = []
     duplicate_lst = []
     no_good_cds_lst = []
     for index, row in df.iterrows():
         accessions = row["Accessions"].split()
-        if test and len(train_accessions.intersection(set(accessions))) > 0:
+        if (
+            train_accessions is not None
+            and len(train_accessions.intersection(set(accessions))) > 0
+        ):
             duplicate_lst.append(index)
-        records = sp.load_from_cache(accessions, cache=cache_viral, filter=False)
+        records = sp.load_from_cache(accessions, cache=cache, filter=False)
         partial_bool = False
         bad_cds_bool = False
         good_cds_bool = False
@@ -61,55 +53,35 @@ def get_bad_indexes(df, test=False):
     }
 
 
-df_train = pd.read_csv(train_data, index_col=0)
-train_accessions = set((" ".join(df_train["Accessions"].values)).split())
-res_train = get_bad_indexes(df_train)
-df_train_fixed = df_train.drop(res_train["partial"] + res_train["no_good_cds"])
-df_train_fixed.to_csv(train_fixed_data)
+if __name__ == "__main__":
+    with tarfile.open(cache_file, "r") as tar:
+        tar.extractall(cache_extract_path)
 
-df_test = pd.read_csv(test_data, index_col=0)
-res_test = get_bad_indexes(df_test, True)
-df_test_fixed = df_test.drop(
-    res_test["partial"] + res_test["no_good_cds"] + res_test["duplicate"]
-)
-df_test_fixed.to_csv(test_fixed_data)
+    df_train = pd.read_csv(train_data, index_col=0)
+    train_accessions = set((" ".join(df_train["Accessions"].values)).split())
+    res_train = get_bad_indexes(df_train, cache_viral)
+    df_train_fixed = df_train.drop(res_train["partial"] + res_train["no_good_cds"])
+    df_train_fixed.to_csv(train_fixed_data)
 
-print("=== Train Summary ===")
+    df_test = pd.read_csv(test_data, index_col=0)
+    res_test = get_bad_indexes(df_test, cache_viral, train_accessions)
+    df_test_fixed = df_test.drop(
+        res_test["partial"] + res_test["no_good_cds"] + res_test["duplicate"]
+    )
+    df_test_fixed.to_csv(test_fixed_data)
 
-print("Partial:", df_train.iloc[res_train["partial"]]["Species"].values)
-print("No Good CDS:", df_train.iloc[res_train["no_good_cds"]]["Species"].values)
+    print("=== Train Summary ===")
 
-print("=== Test Summary ===")
-print("Partial:", df_test.iloc[res_test["partial"]]["Species"].values)
-print("No Good CDS:", df_test.iloc[res_test["no_good_cds"]]["Species"].values)
-print("In training:", df_test.iloc[res_test["duplicate"]]["Species"].values)
+    print("Partial:", df_train.iloc[res_train["partial"]]["Species"].values)
+    print("No Good CDS:", df_train.iloc[res_train["no_good_cds"]]["Species"].values)
 
-removed_from_train = set(df_train.iloc[res_train["partial"]]["Species"].values)
-removed_from_train.update(df_train.iloc[res_train["no_good_cds"]]["Species"].values)
+    print("=== Test Summary ===")
+    print("Partial:", df_test.iloc[res_test["partial"]]["Species"].values)
+    print("No Good CDS:", df_test.iloc[res_test["no_good_cds"]]["Species"].values)
+    print("In training:", df_test.iloc[res_test["duplicate"]]["Species"].values)
 
-assert df_train_fixed.shape == (
-    df_train.shape[0] - len(removed_from_train),
-    df_train.shape[1],
-), f"{df_train_fixed.shape} should equal ({df_train.shape[0]} - {len(removed_from_train)}, {df_train.shape[1]})"
-
-removed_from_test = set(df_test.iloc[res_test["partial"]]["Species"].values)
-removed_from_test.update(df_test.iloc[res_test["no_good_cds"]]["Species"].values)
-removed_from_test.update(df_test.iloc[res_test["duplicate"]]["Species"].values)
-
-assert df_test_fixed.shape == (
-    df_test.shape[0] - len(removed_from_test),
-    df_test.shape[1],
-), f"{df_test_fixed.shape} should equal ({df_test.shape[0]} - {len(removed_from_test)}, {df_test.shape[1]})"
-
-assert not df_train_fixed.duplicated(
-    subset="Accessions"
-).any(), "df_train_fixed contains duplicate accessions"
-assert not df_test_fixed.duplicated(
-    subset="Accessions"
-).any(), "df_test_fixed contains duplicate accessions"
-
-print(
-    "Generated 'fixed' datasets with no partials, no genomes without 'good' CDS, and duplicates dropped from test data:",
-    train_fixed_data,
-    test_fixed_data,
-)
+    print(
+        "Generated 'fixed' datasets with no partials, no genomes without 'good' CDS, and duplicates dropped from test data:",
+        train_fixed_data,
+        test_fixed_data,
+    )

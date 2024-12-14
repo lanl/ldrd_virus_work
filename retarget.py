41d1ffd68f8e9e768f72e5f9def44f4c572dec31
Tyler Reddy <treddy@lanl.gov>
2024-12-13 21:58:39 -0700

    MAINT: bump to test rec 653
    * Bump to Mollentze test set record 653--these
    must all be human-labeled since no remapping
    was needed here.
    
    [ci skip]

--- retarget.py
+++ retarget.py
@@ -1896,7 +1896,7 @@ def main(cache_path):
     df_test = pd.read_csv("viral_seq/data/Mollentze_Holdout.csv")
     y_human_test, y_mammal_test, y_primate_test = retarget(df=df_test,
                                                            cache_path=cache_path,
-                                                           n_records=648)
+                                                           n_records=653)
     np.savez("relabeled_data.npz",
              y_human_train=y_human_train,
              y_mammal_train=y_mammal_train,

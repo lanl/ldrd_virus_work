306c2535f53962f2ac6b7d57d2dafc6f0080293a
Tyler Reddy <treddy@lanl.gov>
2024-12-15 16:27:15 -0700

    ENH: update organism_dict
    * Add 1 new virus:host mapping to `organism_dict`, bringing us
    to Mollentze test set record 688 in the remapping effort.
    
    [ci skip]

--- retarget.py
+++ retarget.py
@@ -1845,6 +1845,8 @@ organism_dict = {
                 # https://www.genome.jp/virushostdb/687381
                 # https://ictv.global/report_9th/ssDNA/Anelloviridae
                 "Torque teno tupaia virus": "non_primate_mammals",
+                # https://www.genome.jp/virushostdb/687353
+                "Torque teno virus 14": "primate",
                 }
 
 
@@ -1932,7 +1934,7 @@ def main(cache_path):
     df_test = pd.read_csv("viral_seq/data/Mollentze_Holdout.csv")
     y_human_test, y_mammal_test, y_primate_test = retarget(df=df_test,
                                                            cache_path=cache_path,
-                                                           n_records=683)
+                                                           n_records=688)
     np.savez("relabeled_data.npz",
              y_human_train=y_human_train,
              y_mammal_train=y_mammal_train,

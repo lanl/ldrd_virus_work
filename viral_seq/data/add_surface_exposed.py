4439aa1fe754f04fbf17a215ef796a8da7e72f7b
Adam <awitmer@lanl.gov>
2025-04-15 17:56:38 -0600

    MAINT: add references for `not_exposed` list
    - add case agnostic string search
    - check all entries containing `ORF` and add references

--- viral_seq/data/add_surface_exposed.py
+++ viral_seq/data/add_surface_exposed.py
@@ -20,39 +20,34 @@ def add_surface_exposed(surface_exposed_df: pd.DataFrame, save_file: str) -> Non
     reference_list = {}
     # non-exhaustive list of non-structural protein names, parts of names (i.e. 'ase' as in protease)
     not_exposed: List[str] = [
-        "ase",
-        "nonstructural",
-        "RNA",
-        "DNA",
-        "polyprotein",
-        "NS",
-        "nsp",
-        "Hel",
-        "Pol",
-        "pol",
-        "3C",
-        "hypothetical",
-        "Hypothetical",
-        "pTP",  # protein tyrosine phosphatase
-        "TP",
-        "ORF",
-        "100k",
-        "IVa2",
-        "tegument",
-        "nsP",
-        "nuclear",
-        "nucleocapsid",
-        "transcription",
-        "non-structural",
-        "nucleo",
-        "core",
-        "regulator",
-        "gag",
-        "Gag",
-        "small T antigen",
-        "large T antigen",
-        "small t antigen",
-        "large t antigen",
+        "ase",  # https://doi.org/10.1016/j.sbi.2006.10.010
+        "nonstructural",  # https://meshb.nlm.nih.gov/record/ui?name=Viral+Nonstructural+Proteins
+        "RNA",  # related to RNA polymerase,
+        "DNA",  # related to DNA polymerase
+        "polyprotein",  # viral polyproteins are proteolytically cleaved during the viral maturation cycle and may contain
+        # mature products that are surface exposed (i.e. prM) but are not considered "surface-exposed" proteins https://doi.org/10.1016/j.coviro.2013.03.009
+        "NS",  # acronym meaning ``non-structural``
+        "nsp",  # acronym for ``non-structural protein``
+        "Hel",  # abbreviation of ``helicase`` https://doi.org/10.1016/S0959-440X(02)00298-1
+        "pol",  # abbreviation of ``Polymerase`` https://doi.org/10.1007/978-1-4614-0980-9_12
+        "3C",  # refers to ``3C proteinase`` of picornaviridae https://doi.org/10.3390/v8030082
+        "hypothetical",  # proteins for which the function is  unknown. https://doi.org/10.1002/cfg.66
+        "pTP",  # precursor terminal protein, involved in viral replication https://doi.org/10.1128/jvi.69.7.4079-4085.1995
+        "TP",  # terminal protein, involved in viral replication https://doi.org/10.1128/jvi.69.7.4079-4085.1995
+        "100k",  # https://www.uniprot.org/uniprotkb/P24932/entry
+        "IVa2",  # https://www.uniprot.org/uniprotkb/P03272/entry
+        "tegument",  # the proteins of herpesviruses that lie between the capsid and viral envelope https://doi.org/10.1128/mmbr.00040-07
+        "nsP",  # aka non-structural protein
+        "nuclear",  # associating with the host nucleus
+        "nucleocapsid",  # referring to the capsid of enveloped viruses, specifically HIV-1 https://viralzone.expasy.org/7
+        "transcription",  # relating to ``transcription factor`` or ``transcription regulator`` https://doi.org/10.1016/j.cell.2020.06.023
+        "non-structural",  # i.e. ``non-structural protein``
+        "nucleo",  # ``ribonucleotide`` or ``nucleocapsid``
+        "core",  # https://www.ncbi.nlm.nih.gov/mesh?Db=mesh&Cmd=DetailsSearch&Term=%22Viral+Core+Proteins%22%5BMeSH+Terms%5D
+        "regulator",  # https://www.ncbi.nlm.nih.gov/mesh?Db=mesh&Cmd=DetailsSearch&Term=%22Viral+Regulatory+and+Accessory+Proteins%22%5BMeSH+Terms%5D
+        "gag",  # associated with late stage viral assembly https://viralzone.expasy.org/5068
+        "small t antigen",  # referring to regulatory proteins of polyomaviruses https://doi.org/10.3390/ijms20163914
+        "large t antigen",  # referring to regulatory proteins of polyomaviruses https://doi.org/10.3390/ijms20163914
     ]
     not_exposed_exceptions: List[str] = [
         "hemagglutinin-neuraminidase",
@@ -81,17 +76,17 @@ def add_surface_exposed(surface_exposed_df: pd.DataFrame, save_file: str) -> Non
     for i, row in enumerate(surface_exposed_df.itertuples()):
         if pd.isna(row.surface_exposed_status):
             if isinstance(row.protein_names, str):
-                protein_query: str = row.protein_names
+                protein_query: str = row.protein_names.lower()
             else:
                 raise TypeError("Invalid protein query type: expected 'str' value.")
-            if any(s in protein_query for s in not_exposed) and not any(
-                s in protein_query for s in not_exposed_exceptions
+            if any(s.lower() in protein_query for s in not_exposed) and not any(
+                s.lower() in protein_query for s in not_exposed_exceptions
             ):
                 response_list[i] = "no"
                 reference_list[i] = "None"
                 remaining -= 1
                 continue
-            if any(s in protein_query for s in exposed):
+            if any(s.lower() in protein_query for s in exposed):
                 response_list[i] = "yes"
                 reference_list[i] = "None"
                 remaining -= 1
--- viral_seq/data/check_entries.py
+++ viral_seq/data/check_entries.py
@@ -18,8 +18,11 @@ def check_entries(
     df_file: str
         file name for saving modified dataframe
     """
+    all_terms = "|".join(check_list)
     cross_reference = surface_exposed_df[
-        surface_exposed_df["protein_names"].isin(check_list)
+        surface_exposed_df["protein_names"].str.contains(
+            all_terms, case=False, na=False
+        )
     ]
     # count the number of entries that do not have references
     remaining = pd.isna(cross_reference["reference"]).sum()
@@ -49,61 +52,8 @@ def check_entries(
 
 
 if __name__ == "__main__":
-    purge_list = [
-        "1B(VP2)",
-        "1C(VP3)",
-        "1D(VP1)",
-        "Envelope surface glycoprotein gp120",
-        "Envelope surface glycoprotein gp160, precursor",
-        "PreM protein",
-        "VP1",
-        "VP1 protein",
-        "VP2",
-        "VP2 protein",
-        "VP3",
-        "VP3 protein",
-        "envelope glycoprotein E1",
-        "envelope glycoprotein E2",
-        "envelope protein",
-        "envelope protein E",
-        "membrane glycoprotein M",
-        "membrane glycoprotein precursor prM",
-        "membrane protein M",
-        "hemagglutinin-neuraminidase",
-        "envelope glycoprotein 150",
-        "envelope glycoprotein B",
-        "envelope glycoprotein E",
-        "envelope glycoprotein G",
-        "envelope glycoprotein H",
-        "envelope glycoprotein M",
-        "envelope glycoprotein UL37",
-        "envelope protein",
-        "envelope protein E",
-        "envelope protein UL20",
-        "envelope protein UL43",
-        "membrane glycoprotein",
-        "membrane glycoprotein UL16",
-        "membrane glycoprotein UL40",
-        "membrane protein UL120",
-        "membrane protein UL124",
-        "membrane protein UL20",
-        "membrane protein UL45",
-        "membrane protein UL56",
-        "membrane protein US12",
-        "membrane protein US15",
-        "membrane protein US30",
-        "membrane protein V1",
-        "hexon",
-        "hexon protein",
-        "3A",
-        "3A protein",
-        "Asp",  # HIV-1 Antisense Protein, https://doi.org/10.1128/jvi.00574-19
-        "CR1-beta",
-    ]
+    purge_list = ["ORF"]
 
     df_file = "surface_exposed_df.csv"
     surface_exposed_df = pd.read_csv("surface_exposed_df.csv")
-    import pdb
-
-    pdb.set_trace()
     check_entries(surface_exposed_df, purge_list, df_file)

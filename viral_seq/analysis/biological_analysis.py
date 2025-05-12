99dcc7f958dfa8deee0defcb2f70a4894a6ef1aa
Adam <awitmer@lanl.gov>
2025-05-12 14:58:57 -0600

    MAINT: address reviewer comments
    - remove rounding from testing assertion
    - fix data structure handling
    - improve logic for slicing peptide in `hydrophobicity_score`
    - move ValueError assertion in `hydrophobicity_score`
    - check/fix spelling errors using codespell
    - fix mypy type hints in function signature and docstrings
    - fix doc indentation inconsistencies to align with numpy conventions

--- viral_seq/analysis/biological_analysis.py
+++ viral_seq/analysis/biological_analysis.py
@@ -1,8 +1,7 @@
 import pandas as pd
 from Bio.SeqUtils.ProtParam import ProteinAnalysis
 import itertools
-from collections import defaultdict
-from typing import Literal
+from typing import Literal, Optional
 
 
 def pc_to_aa(
@@ -13,16 +12,16 @@ def pc_to_aa(
 
     Parameters:
     -----------
-        kmer_peptide: str
-                PC-kmer peptide to translate. input should be just
-                a string of numbers without ``kmer_PC_`` prefix
-        mapping_method: str
-                mapping scheme for translating PC to AA kmers
+    kmer_peptide: str
+        PC-kmer peptide to translate. input should be just
+        a string of numbers without ``kmer_PC_`` prefix
+    mapping_method: Literal["jurgen_schmidt", "shen_2007"]
+        mapping scheme for translating PC to AA kmers
 
-        Returns:
-        --------
-        aa_seq_list: list
-                list of AA sequences translated from PC kmers
+    Returns:
+    --------
+    aa_seq_list: list
+        list of AA sequences translated from PC kmers
     """
     # TODO: currently there is no support for 'J' in hydrophobicity score translations (see issue #121)
     translations = {
@@ -58,7 +57,7 @@ def pc_to_aa(
 
 def hydrophobicity_score(
     kmer_list: list[str],
-    mapping_method: Literal["jurgen_schmidt", "shen_2007", None] = None,
+    mapping_method: Optional[Literal["jurgen_schmidt", "shen_2007"]] = None,
 ) -> pd.DataFrame:
     """
     calculate the hydrophobicity score of input kmers using
@@ -66,48 +65,44 @@ def hydrophobicity_score(
 
     Parameters:
     -----------
-    kmer_list: list
-            list containing topN kmers including prefixes (i.e. ``kmer_AA_`` or ``kmer_PC_``)
-    mapping_method: str
-            mapping method for determing AA kmers associated with PC kmer inputs
+    kmer_list: list[str]
+        list containing topN kmers including prefixes (i.e. ``kmer_AA_`` or ``kmer_PC_``)
+    mapping_method: Optional[Literal["jurgen_schmidt", "shen_2007"]]
+        mapping method for determining AA kmers associated with PC kmer inputs
 
     Returns:
     --------
     kmer_gravy_df: pd.DataFrame
-            pandas dataframe containing kmer inputs, translated peptides and hydrophobicity scores
+        pandas dataframe containing kmer inputs, translated peptides and hydrophobicity scores
     """
 
-    # check that if PC kmers in list, a mapping method is provided
-    if any("kmer_PC_" in s for s in kmer_list) and mapping_method is None:
-        raise ValueError(
-            "Please provide a mapping method ('jurgen_schmidt' or 'shen_2007') for performing PC-AA translations."
-        )
-
-    kmer_gravy = defaultdict(list)
+    kmer_gravy = []
     # iterate through kmers
     for kmer in kmer_list:
         # determine status of kmer as PC or AA
         if kmer[:8] == "kmer_AA_":
-            kmer_status = "AA"
+            aa_peptides = [kmer[8:]]
         else:
-            kmer_status = "PC"
-
-        # if PC kmer, find all AA kmer maps corresponding to PC kmer
-        if kmer_status == "PC":
+            # if PC kmer, find all AA kmer maps corresponding to PC kmer
             pc_peptide = kmer[8:]
             # TODO: determine whether translating pc to aa kmers will be necessary
             # after merge of !115 (tracing kmers) vs. using explicit matches from dataset
             if mapping_method is not None:
                 aa_peptides = pc_to_aa(pc_peptide, mapping_method)
-        else:
-            aa_peptides = [kmer[8:]]
+            # check that if PC kmers in list, a mapping method is provided
+            else:
+                raise ValueError(
+                    "Please provide a mapping method ('jurgen_schmidt' or 'shen_2007') for performing PC-AA translations."
+                )
 
         # calculate hydrophobicity scores for all kmers
         for aa_peptide in aa_peptides:
             protein_info = ProteinAnalysis(aa_peptide)
-            kmer_gravy[kmer].append((aa_peptide, protein_info.gravy()))
+            kmer_gravy.append((kmer, aa_peptide, protein_info.gravy()))
 
-    kmer_gravy_df = pd.DataFrame.from_dict(kmer_gravy, orient="index")
+    kmer_gravy_df = pd.DataFrame.from_records(kmer_gravy).rename(
+        columns={0: "kmer", 1: "peptide", 2: "score"}
+    )
     # reverse order of gravy scores to match FIC plot ranking
     kmer_gravy_df = kmer_gravy_df[::-1]
     return kmer_gravy_df
--- viral_seq/tests/test_bio.py
+++ viral_seq/tests/test_bio.py
@@ -1,6 +1,7 @@
 from viral_seq.analysis import biological_analysis as ba
 import pytest
 from pandas.testing import assert_frame_equal
+from numpy.testing import assert_allclose
 import pandas as pd
 
 
@@ -10,21 +11,43 @@ import pandas as pd
         (
             ["kmer_AA_AGC"],
             None,
-            {0: {"kmer_AA_AGC": ("AGC", 1.3)}},
+            {"kmer": {0: "kmer_AA_AGC"}, "peptide": {0: "AGC"}, "score": {0: 1.3}},
         ),
         # test case for PC-kmer with "jurgen_schmidt" mapping method
         (
             ["kmer_PC_057"],
             "jurgen_schmidt",
             {
-                0: {"kmer_PC_057": ("AKF", 0.2333333333333334)},
-                1: {"kmer_PC_057": ("AKW", -0.9999999999999999)},
-                2: {"kmer_PC_057": ("ARF", 0.033333333333333215)},
-                3: {"kmer_PC_057": ("ARW", -1.2)},
-                4: {"kmer_PC_057": ("GKF", -0.5)},
-                5: {"kmer_PC_057": ("GKW", -1.7333333333333334)},
-                6: {"kmer_PC_057": ("GRF", -0.7000000000000002)},
-                7: {"kmer_PC_057": ("GRW", -1.9333333333333336)},
+                "kmer": {
+                    7: "kmer_PC_057",
+                    6: "kmer_PC_057",
+                    5: "kmer_PC_057",
+                    4: "kmer_PC_057",
+                    3: "kmer_PC_057",
+                    2: "kmer_PC_057",
+                    1: "kmer_PC_057",
+                    0: "kmer_PC_057",
+                },
+                "peptide": {
+                    7: "GRW",
+                    6: "GRF",
+                    5: "GKW",
+                    4: "GKF",
+                    3: "ARW",
+                    2: "ARF",
+                    1: "AKW",
+                    0: "AKF",
+                },
+                "score": {
+                    7: -1.9333333333333336,
+                    6: -0.7000000000000002,
+                    5: -1.7333333333333334,
+                    4: -0.5,
+                    3: -1.2,
+                    2: 0.033333333333333215,
+                    1: -0.9999999999999999,
+                    0: 0.2333333333333334,
+                },
             },
         ),
         # test case for PC-kmer with "shen_2007" mapping method
@@ -32,18 +55,48 @@ import pandas as pd
             ["kmer_PC_123"],
             "shen_2007",
             {
-                0: {"kmer_PC_123": ("ACF", 2.3666666666666667)},
-                1: {"kmer_PC_123": ("ACL", 2.6999999999999997)},
-                2: {"kmer_PC_123": ("ACI", 2.9333333333333336)},
-                3: {"kmer_PC_123": ("ACP", 0.8999999999999999)},
-                4: {"kmer_PC_123": ("GCF", 1.6333333333333335)},
-                5: {"kmer_PC_123": ("GCL", 1.9666666666666668)},
-                6: {"kmer_PC_123": ("GCI", 2.1999999999999997)},
-                7: {"kmer_PC_123": ("GCP", 0.16666666666666666)},
-                8: {"kmer_PC_123": ("VCF", 3.1666666666666665)},
-                9: {"kmer_PC_123": ("VCL", 3.5)},
-                10: {"kmer_PC_123": ("VCI", 3.733333333333333)},
-                11: {"kmer_PC_123": ("VCP", 1.7)},
+                "kmer": {
+                    11: "kmer_PC_123",
+                    10: "kmer_PC_123",
+                    9: "kmer_PC_123",
+                    8: "kmer_PC_123",
+                    7: "kmer_PC_123",
+                    6: "kmer_PC_123",
+                    5: "kmer_PC_123",
+                    4: "kmer_PC_123",
+                    3: "kmer_PC_123",
+                    2: "kmer_PC_123",
+                    1: "kmer_PC_123",
+                    0: "kmer_PC_123",
+                },
+                "peptide": {
+                    11: "VCP",
+                    10: "VCI",
+                    9: "VCL",
+                    8: "VCF",
+                    7: "GCP",
+                    6: "GCI",
+                    5: "GCL",
+                    4: "GCF",
+                    3: "ACP",
+                    2: "ACI",
+                    1: "ACL",
+                    0: "ACF",
+                },
+                "score": {
+                    11: 1.7,
+                    10: 3.733333333333333,
+                    9: 3.5,
+                    8: 3.1666666666666665,
+                    7: 0.16666666666666666,
+                    6: 2.1999999999999997,
+                    5: 1.9666666666666668,
+                    4: 1.6333333333333335,
+                    3: 0.8999999999999999,
+                    2: 2.9333333333333336,
+                    1: 2.6999999999999997,
+                    0: 2.3666666666666667,
+                },
             },
         ),
         # test case for mix of PC and AA kmers
@@ -51,15 +104,33 @@ import pandas as pd
             ["kmer_AA_AGC", "kmer_PC_012"],
             "jurgen_schmidt",
             {
-                0: {
-                    "kmer_PC_012": ("ACS", 1.1666666666666667),
-                    "kmer_AA_AGC": ("AGC", 1.3),
+                "kmer": {
+                    6: "kmer_PC_012",
+                    5: "kmer_PC_012",
+                    4: "kmer_PC_012",
+                    3: "kmer_PC_012",
+                    2: "kmer_PC_012",
+                    1: "kmer_PC_012",
+                    0: "kmer_AA_AGC",
                 },
-                1: {"kmer_PC_012": ("ACT", 1.2), "kmer_AA_AGC": None},
-                2: {"kmer_PC_012": ("ACY", 1.0), "kmer_AA_AGC": None},
-                3: {"kmer_PC_012": ("GCS", 0.43333333333333335), "kmer_AA_AGC": None},
-                4: {"kmer_PC_012": ("GCT", 0.46666666666666673), "kmer_AA_AGC": None},
-                5: {"kmer_PC_012": ("GCY", 0.26666666666666666), "kmer_AA_AGC": None},
+                "peptide": {
+                    6: "GCY",
+                    5: "GCT",
+                    4: "GCS",
+                    3: "ACY",
+                    2: "ACT",
+                    1: "ACS",
+                    0: "AGC",
+                },
+                "score": {
+                    6: 0.26666666666666666,
+                    5: 0.46666666666666673,
+                    4: 0.43333333333333335,
+                    3: 1.0,
+                    2: 1.2,
+                    1: 1.1666666666666667,
+                    0: 1.3,
+                },
             },
         ),
     ],
@@ -125,7 +196,7 @@ def test_pc_to_aa(peptide, mapping_method, exp_out):
 def test_gravy_calculation(kmer_in, exp_gravy):
     # sanity check for gravy hydrophobicity using known peptide-scores from literature
     gravy_score = ba.hydrophobicity_score(kmer_in)
-    assert round(gravy_score.iloc[0][0][1], 2) == exp_gravy
+    assert_allclose(gravy_score.score, exp_gravy, rtol=0.02)
 
 
 def test_hydrophobicity_score_error():

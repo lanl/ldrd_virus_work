95cb16c8464ac3a55420173182cf5d6245f2d9bb
Tyler Reddy <treddy@lanl.gov>
2026-01-06 12:29:59 -0700

    ENH: include full justification blocks

--- human_readable_targets.py
+++ human_readable_targets.py
@@ -10,30 +10,38 @@ import pandas as pd
 justification_dict = defaultdict(dict)
 with open("retarget.py") as infile:
     started = False
-    justification_found = False
     lines = infile.readlines()
     counter = 0
-    for idx, line in enumerate(lines):
-        stripped_line = line.strip()
+    line_idx = 0
+    while line_idx < 3151:
+        stripped_line = lines[line_idx].strip()
         if stripped_line.startswith("organism_dict"):
             started = True
-        if started and stripped_line.startswith("# http") and not justification_found:
-            justification_dict[counter]["justification"] = stripped_line[2:]
-            justification_found = True
-        if started and justification_found:
-            if stripped_line.count('"') == 4:
-                virus_name, host_label = stripped_line.split(":")
-                host_label = host_label.replace(",", "")
-                justification_dict[counter]["virus name"] = virus_name
-                justification_dict[counter]["host label"] = host_label
-                counter += 1
-                justification_found = False
+        if started and stripped_line.startswith("#"):
+            comment_block = ""
+            for line_index in range(line_idx, len(lines)):
+                stripped_sub_line = lines[line_index].strip()
+                if stripped_sub_line.startswith("#"):
+                    stripped_sub_line += "<br>"
+                    comment_block += stripped_sub_line
+                    line_idx += 1
+                else:
+                    break
+            justification_dict[counter]["justification"] = comment_block
+            continue
+        if started and stripped_line.count('"') == 4:
+            virus_name, host_label = stripped_line.split(":")
+            host_label = host_label.replace(",", "")
+            justification_dict[counter]["virus name"] = virus_name
+            justification_dict[counter]["host label"] = host_label
+            counter += 1
+        line_idx += 1
 
 df = pd.DataFrame.from_dict(justification_dict,
                             orient="index",
                             columns=["virus name", "host label", "justification"])
 print(df)
-html = df.to_html(render_links=True)
+html = df.to_html(render_links=True, escape=False, justify='left')
 with open("table.html", "w") as outfile:
     outfile.writelines(html)
 
